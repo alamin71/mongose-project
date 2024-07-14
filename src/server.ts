a6e1b54166eefee7ad6fb4bfc17aaf1d044569fa
Al-Amin <alamin50cse.miu@gmail.com>
2024-07-14 21:26:26 +0600

    Create interface, Schema, Model, Route , Service And Controller

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,10 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example app is one of the listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
   }
 }
+main();
